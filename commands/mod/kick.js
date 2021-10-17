3595dce926e30bbf098ef693ee7c50a23344c854
Idlidev <idliyout@gmail.com>
2021-10-17 14:38:49 +0530

    kick command

---
+++ commands/mod/kick.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Kick a member.')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user to kick.')
+                .setRequired(true)),
+	async execute(interaction) {
+        const member = interaction.options.getMember('user');
+        member.kick();
+
+	    interaction.reply(`${member} has been kicked.`);
+	},
+};
