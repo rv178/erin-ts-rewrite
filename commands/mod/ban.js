b5fd56360490f9915ab19256434c14a63f26b2a7
Idli <69449141+Idlidev@users.noreply.github.com>
2021-10-17 14:53:12 +0530

    Update ban.js

---
+++ commands/mod/ban.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ban')
+		.setDescription('Ban a member.')
+        .addStringOption(option => 
+            option.setName('user')
+                .setDescription('The user to ban.')
+                .setRequired(true)),
+	async execute(interaction) {
+        const member = interaction.options.getMember('user');
+        guild.members.ban(member);
+
+	    interaction.reply(`${member} has been banned.`);
+	},
+};
